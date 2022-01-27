806567c037460ade29dc6f024f2c2e0faeb7f430
Гусаченко Сергій <gusachenko02@gmail.com>
2022-01-27 17:49:11 +0200

    fix

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import PropTypes from "prop-types";
 import React, { Component } from "react";
 import React, { Component } from "react";
 import { StyleSheet, View, ViewPropTypes } from "react-native";
